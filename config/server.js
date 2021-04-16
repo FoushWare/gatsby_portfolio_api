22e664dc1552dc01bc0e92b43015adafdd010483
foushware <mr.fouad992@gmail.com>
2021-04-16 06:15:50 +0200

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '07df2473c228a2982432544d6636ee70'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4d671e94-246b-4afe-9afe-52ecef37a5c2'
+};
