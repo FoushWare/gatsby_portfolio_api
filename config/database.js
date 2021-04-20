b14baddac98691b005a16c09a0a75c4dbada1753
foushware <mr.fouad992@gmail.com>
2021-04-20 04:17:45 +0200

    change the config of database to use mongodb and deploy to heroku

--- config/database.js
+++ config/database.js
@@ -1,14 +1,33 @@
+//Sequlite
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
+
+// Production
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: 'mongoose',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        uri: env('DATABASE_URI'),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: true,
       },
     },
   },
